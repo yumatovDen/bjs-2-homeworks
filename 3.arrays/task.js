d10bbb8007d1465499f67c0856e1a6003fe72c52
Denis Yumatov <135641158+yumatovDen@users.noreply.github.com>
2023-09-10 19:56:52 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
+// Задание 1
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i])
 }
 
+// Задание 2
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, usersAge) => acc + age/usersAge.length, 0)
+    return result;
 }

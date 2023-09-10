51c023b63ce84760f5876fb26af5f692ed8ffb51
Denis Yumatov <135641158+yumatovDen@users.noreply.github.com>
2023-09-10 19:38:42 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.marks = [];
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.hasOwnProperty("marks") === false) {
+    return "Пользователь отчислен";
+  } else {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.hasOwnProperty("marks") === false || this.marks.length === 0) {
+    return 0;
+  } else {
+    return this.marks.reduce((acc, mark) => acc + mark/this.marks.length, 0)
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject && delete this.marks;
+  this.excluded = reason;
 }
